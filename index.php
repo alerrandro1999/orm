7e968855b372deef471c40993d2fd33599c87f0e
alerrandro borges <alerrandrokaton@gmail.com>
2022-07-26 16:51:14 -0300

    criando classe do select

--- index.php
+++ index.php
@@ -4,7 +4,14 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Alerr\Orm\Environment\LoadVar;
 
+use Alerr\Orm\OrmClass\Select\Select;
+
 LoadVar::load(__DIR__);
 
+$select = new Select;
 
+$dados = $select->selectTable('persistings');
 
+echo '<pre>';
+print_r($dados);
+echo '</pre>';
